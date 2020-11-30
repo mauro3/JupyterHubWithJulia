59d095ba2c48d02bb0d69ac37fd605d4c9d1eeae
Mauro Werder <mauro3@runbox.com>
2020-11-30 07:55:16 +0100

    copy_to_users.sh update

--- copy_to_users.sh
+++ copy_to_users.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 #
-# This copies (recursively) files/dirs to all users.
+# This copies (recursively) files/dirs to all users and to /etc/skel
 
 mkdir /tmp/transfer
 cp -r $1 /tmp/transfer
 chmod -R a+rx /tmp/transfer/$1
+
+cp -r $1 /etc/skel
 
 cd /home
 export users=jupyter-*
